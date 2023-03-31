3ff19c5eecffbd18f45738f69a0395c65814eba7
Odjoutchone Sylvestre Tokou <odjoutchone@yahoo.fr>
2023-03-31 10:12:35 -0400

    Adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 =31;
+let num2 = 2
+let  multiply = num1 * num2;
+
+let random = 8;
+Math.random (8) * 1;
+
+let num3 = 44 ;
+let num4 = 10 ;
+let mod = num3 % num4;
+
+let max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ,12, 13, 14, 15, 16, 17, 18, 19, 20);
